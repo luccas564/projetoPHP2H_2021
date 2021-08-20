fcec739b3c8fe76851689642db9d13694ba43d90
Lucas da silva ribeiro <luccas564@users.noreply.github.com>
2021-08-19 22:14:32 -0300

    atualização

---
+++ segundo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<?php 
+    # testando comentario de hash marks
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - testando comentário</title>
+</head>
+<body>
+    <?php
+    /* 
+        comentário de linha
+     */
+        echo "Segundo exemplo de comentário";
+        $data = "19 de agosto de 2021";
+    ?>
+    <p> 
+    Este parágrafo esta sendo interpretado em HTML
+    </p>
+    Arquivo criado em php <?php echo $data; ?>
+</body>
+</html>
